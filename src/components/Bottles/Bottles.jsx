88028d8fb3d157fd1e551cfccff4bde54467a737
Dipu Haider <dipuhaider@gmail.com>
2023-09-12 15:07:30 +0600

    Add to cart, remove from cart, data in localstorage

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Bottle from "../Bottle/Bottle";
-import './Bottles.css'
+import './Bottles.css';
+import { addToLS, getStoredCart, removeFromLS } from "../../utilities/localstorage";
+import Cart from "../Cart/Cart";
 
 const Bottles = () => {
 
@@ -12,25 +14,55 @@ const Bottles = () => {
         fetch('bottles.json').then(res => res.json()).then(data => setBottles(data));
     }, []); 
 
+    // load cart from local storage
+    useEffect(() => {
+        console.log('called the useEffect', bottles.length)
+        if (bottles.length) {
+            const storedCart = getStoredCart();
+            console.log(storedCart, bottles);
+            const savedCart = [];
+            for(const id of storedCart){
+                console.log(id);
+                const bottle = bottles.find(bottle => bottle.id === id);
+                if(bottle){
+                    savedCart.push(bottle)
+                }
+            }
+
+            console.log('saved cart', savedCart)
+            setCart(savedCart);
+
+        }
+    }, [bottles])
+
     const handleAddToCart = bottle => {
         // console.log('bottle going to be added');
         //console.log({bottle});
         const newCart = [...cart, bottle];
         setCart(newCart);
+        addToLS(bottle.id);
+    }
+
+    const handleRemoveFromCart = id => {
+        // visual cart remove
+        const remainingCart = cart.filter(bottle => bottle.id !== id);
+        setCart(remainingCart);
+        // remove from LS 
+        removeFromLS(id);
     }
 
     return (
         <div>
             <h2>Bottles Available: {bottles.length}</h2>
-            <h4>In Cart: {cart.length}</h4>
+            <Cart cart={cart} handleRemoveFromCart={handleRemoveFromCart}></Cart>
             <div className="bottle-container">
-            {
-                bottles.map(bottle => <Bottle 
-                    key={bottle.id} 
-                    bottle={bottle}
-                    handleAddToCart ={handleAddToCart}
+                {
+                    bottles.map(bottle => <Bottle
+                        key={bottle.id}
+                        bottle={bottle}
+                        handleAddToCart={handleAddToCart}
                     ></Bottle>)
-            }
+                }
             </div>
         </div>
     );
