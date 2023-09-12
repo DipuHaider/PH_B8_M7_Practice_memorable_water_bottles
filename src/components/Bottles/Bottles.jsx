2deb1869d162e5b031019f5864f9421a672352a6
Dipu Haider <dipuhaider@gmail.com>
2023-09-12 12:00:13 +0600

    Update Bottles.jsx

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const Bottles = () => {
+
+    const [bottles, setBottles] = useState([]);
+
+    useEffect(() => {
+        fetch('bottles.json').then(res => res.json()).then(data => setBottles(data));
+    })
+
     return (
         <div>
-            <h2>Bottles Here</h2>
+            <h2>Bottles Here: {bottles.length}</h2>
         </div>
     );
 };
